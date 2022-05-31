04f83a9fea9bf157ce7d09da5514d905bfaad6b1
Jorge <jorg.palma@duocuc.cl>
2022-05-31 01:09:06 -0400

    SADASD

--- MusicPro/urls.py
+++ MusicPro/urls.py
@@ -19,10 +19,9 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.conf.urls import url
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
     path('', include('core.urls')),
     path('accounts/', include('django.contrib.auth.urls'))
 ]
